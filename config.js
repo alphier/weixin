ffd34e3de734fffe2eb920c13fc4a86e775e531a
alphy_ren <alphy.ren@gmail.com>
2015-01-26 17:40:54 +0800

    add ;

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ config.weixin = {};
 config.weixin.appid = 'wx60761e8feb7cc54f';
 config.weixin.appsecret = 'c4eaf5d85981ae48cab4659df1f9cae5';
 config.weixin.token = 'alphy1221';
-config.weixin.server = 'https://api.weixin.qq.com/cgi-bin'
+config.weixin.server = 'https://api.weixin.qq.com/cgi-bin';
 
 module.exports = config;
